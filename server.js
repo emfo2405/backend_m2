810a801e3e128f22823ff91107d3735100b54d5e
emfo2405 <emfo2405@student.miun.se>
2025-04-27 09:42:33 +0200

    koppling till databasen skapad

--- server.js
+++ server.js
@@ -4,3 +4,17 @@ const mysql = require("mysql");
 const cors = require("cors");
 
 const app = express();
+
+const port = process.env.PORT || 3000;
+
+//Kontroll av koppling till databas
+console.log("Database:", process.env.DB_DATABASE);
+
+//Skapar en koppling till databasen
+const connection = mysql.createConnection( {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
+});
