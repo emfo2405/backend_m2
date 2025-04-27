77ee8c7546f65d504c0ef4b4c504c784537c9175
emfo2405 <emfo2405@student.miun.se>
2025-04-27 13:21:10 +0200

    tabell skapad i databas

--- install.js
+++ install.js
@@ -7,12 +7,33 @@ async function install() {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT,
+        ssl: {
+            rejectUnauthorized: false
+        }
     });
 
     try {
         await client.connect();
+        console.log("Anslutning till databas lyckades");
+        const sql = `
+            CREATE TABLE experience(
+            id SERIAL PRIMARY KEY,
+            companyname VARCHAR(25) NOT NULL,
+            jobtitle VARCHAR(20) NOT NULL,
+            location VARCHAR(25) NOT NULL,
+            startdate DATE NOT NULL,
+            enddate DATE NOT NULL,
+            description TEXT NOT NULL);
+        `;
+        await client.query(sql);
+        console.log("Tabell över arbetslivserfarenheter skapade");
+            
     } catch(error) {
         console.error(error);
+    } finally {
+        await client.end();
     }
 }
+
+install();
--- server.js
+++ server.js
@@ -1,25 +1,11 @@
-require("dotenv").config();
+/*require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 
-const app = express();
+const app = express(); */
 
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
 
-
-
-
-//Meddelanden ifall kopplingen inte fungerar eller om kopplingen lyckas
-connection.connect((err) => {
-    if(err) {
-        console.log("Koppling till databasen misslyckades " + err);
-    } else {
-    console.log("Koppling till databasen lyckades")
-    }
-});
-
-app.use(cors());
-app.use(express.json());
 
 //Skapar routes
