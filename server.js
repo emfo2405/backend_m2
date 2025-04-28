b09d4678cbecce33347bce6e26f8e91e15018f70
emfo2405 <emfo2405@student.miun.se>
2025-04-28 10:02:18 +0200

    Fel fixat

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 const client = new Client ({
     host: process.env.DB_HOST,
